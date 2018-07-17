e87294c83d5f82636fa0f8720e665c60cb477483
cefimenda <jeffymenda@gmail.com>
2018-07-17 15:41:20 -0400

    scrapes only 3 at a time

--- node_scrapeBackstage.js
+++ node_scrapeBackstage.js
@@ -128,17 +128,24 @@ let scrape = async () => {
 
     // Scrape
     var funcList = []
-    for(var i=1;i<6;i++){
+    for(var i=1;i<4;i++){
         funcList.push(scrapePage(i,auditionList,browser))
     }
 
     const responses = await Promise.all(funcList); //--> to run all scraping simultaneously
 
     var funcList = []
-    for(var i=6;i<11;i++){
+    for(var i=4;i<7;i++){
         funcList.push(scrapePage(i,auditionList,browser))
     }
     const responses2 = await Promise.all(funcList); //--> to run all scraping simultaneously
+
+    var funcList = []
+    for(var i=7;i<11;i++){
+        funcList.push(scrapePage(i,auditionList,browser))
+    }
+    const responses3 = await Promise.all(funcList); //--> to run all scraping simultaneously
+
     browser.close();     //close browser
     console.log(auditionList)
     auditions.backstage = auditionList
